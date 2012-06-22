0b24e6b665a536e735c613a1854454d15460c81f
Ronnie Taylor <ronnie@ronnietaylor.com>
2012-06-22 15:13:21 -0400

    removed explicit dev environment declaration

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ configure do
 	enable :sessions
 	set :run, false
 	set :raise_errors, true
-	set :environment, :development
   set :haml, :format => :html5
   set :scss, :style => :compact, :debug_info => false
 end
